65066c372a731b949ec8cf2c2df0f1ed8cc8b350
张旭 <17610588786@163.com>
2021-05-14 18:17:28 +0800

    RadioButton 完结

--- app/src/main/java/com/smart4s/app/merchant/helloworld/MainActivity.java
+++ app/src/main/java/com/smart4s/app/merchant/helloworld/MainActivity.java
@@ -12,39 +12,52 @@ public class MainActivity extends AppCompatActivity {
     private Button mBtnTextView;
     private Button mBtnButton;
     private Button mBtnEditText;
+    private Button mBtnRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBtnTextView = this.<Button>findViewById(R.id.btn_textview);
-        mBtnTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 跳转到TextView演示界面
-                Intent intent = new Intent(MainActivity.this, TextViexActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        mBtnButton = findViewById(R.id.btn_button);
-        mBtnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 跳转到Button演示界面
-                Intent intent = new Intent(MainActivity.this, ButtonActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        mBtnButton = this.<Button>findViewById(R.id.btn_button);
         mBtnEditText = this.<Button>findViewById(R.id.btn_editText);
-        mBtnEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 跳转到EditText演示界面
-                Intent intent = new Intent(MainActivity.this, EditTextActivity.class);
-                startActivity(intent);
+        mBtnRadioButton = this.<Button>findViewById(R.id.btn_radioButton);
+        setListener();
+    }
+
+    private void setListener(){
+        OnClick onClick = new OnClick();
+        mBtnTextView.setOnClickListener(onClick);
+        mBtnButton.setOnClickListener(onClick);
+        mBtnEditText.setOnClickListener(onClick);
+        mBtnRadioButton.setOnClickListener(onClick);
+    }
+
+    private class OnClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = null;
+            switch (view.getId()){
+                case R.id.btn_textview:
+                    // 跳转到TextView演示界面
+                    intent = new Intent(MainActivity.this, TextViexActivity.class);
+                    break;
+                case R.id.btn_button:
+                    // 跳转到Button演示界面
+                    intent = new Intent(MainActivity.this, ButtonActivity.class);
+                    break;
+                case R.id.btn_editText:
+                    // 跳转到EditText演示界面
+                    intent = new Intent(MainActivity.this, EditTextActivity.class);
+                    break;
+                case R.id.btn_radioButton:
+                    // 跳转到RadioButton演示界面
+                    intent = new Intent(MainActivity.this, RadioButtonActivity.class);
+                    break;
             }
-        });
+            startActivity(intent);
+
+        }
     }
 }
