9c7fa36cd734887d536c4f1850fdf17afe1afc97
张旭 <17610588786@163.com>
2021-05-14 15:48:40 +0800

    button 完结

--- app/src/main/java/com/smart4s/app/merchant/helloworld/ButtonActivity.java
+++ app/src/main/java/com/smart4s/app/merchant/helloworld/ButtonActivity.java
@@ -1,14 +1,40 @@
 package com.smart4s.app.merchant.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ButtonActivity extends AppCompatActivity {
 
+    private Button mBtn3;
+    private TextView mTv1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+        mBtn3 = this.<Button>findViewById(R.id.btn_3);
+        mBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonActivity.this, "Btn3被点击了", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mTv1= this.<TextView>findViewById(R.id.tv_1);
+        mTv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonActivity.this, "tv1被点击了", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void showToast(View view){
+        Toast.makeText(this, "Btn4被点击了", Toast.LENGTH_SHORT).show();
     }
 }
