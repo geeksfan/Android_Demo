b3226c5535ccf48895d5b04294e62324d3950476
张旭 <17610588786@163.com>
2021-05-15 08:22:43 +0800

    CheckBox 完结

--- app/src/main/java/com/smart4s/app/merchant/helloworld/CheckBoxActivity.java
+++ app/src/main/java/com/smart4s/app/merchant/helloworld/CheckBoxActivity.java
@@ -1,14 +1,36 @@
 package com.smart4s.app.merchant.helloworld;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckBoxActivity extends AppCompatActivity {
 
+    private CheckBox mCb5, mCb6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_box);
+        mCb5 = findViewById(R.id.cb_5);
+        mCb6 = findViewById(R.id.cb_6);
+
+        mCb5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(CheckBoxActivity.this, b ? "5已选中" : "5未选中", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mCb6.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(CheckBoxActivity.this, b ? "6已选中" : "6未选中", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
